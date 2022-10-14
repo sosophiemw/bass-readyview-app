002d847ab87d382fecbb15a422718dcafec1420f
David Ward <david.a.ward@duke.edu>
2022-10-13 21:01:04 -0400

    Code to read in entire line

--- serial_port_access.py
+++ serial_port_access.py
@@ -7,11 +7,14 @@ ser.write("[1D100\r".encode('utf-8'))
 # ser.open()
 while True:
     # time.sleep(0.1)
-    s = ser.read(1)
-    hex_string = s.hex()
-    dec = int(hex_string, 16)
-    if dec < 128:
-        ascii = chr(dec)
-    else:
-        ascii = ""
-    print("Hex {} / Decimal {} / Char {}".format(hex_string, dec, ascii))
+    # s = ser.read(1)
+    # hex_string = s.hex()
+    # dec = int(hex_string, 16)
+    # if dec < 128:
+    #     ascii = chr(dec)
+    # else:
+    #     ascii = ""
+    # print("Hex {} / Decimal {} / Char {}".format(hex_string, dec, ascii))
+
+    s = ser.readline()
+    print(s)
