e9e043b8e0b88a5b93d1019b218fc78f279b096e
David Ward <david.a.ward@duke.edu>
2022-10-13 20:59:08 -0400

    Basic code to read from serial port

---
+++ serial_port_access.py
@@ -0,0 +1,17 @@
+import time
+import serial
+
+ser = serial.Serial(port='COM4', baudrate=115200, bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+ser.write("[1D100\r".encode('utf-8'))
+# ser.open()
+while True:
+    # time.sleep(0.1)
+    s = ser.read(1)
+    hex_string = s.hex()
+    dec = int(hex_string, 16)
+    if dec < 128:
+        ascii = chr(dec)
+    else:
+        ascii = ""
+    print("Hex {} / Decimal {} / Char {}".format(hex_string, dec, ascii))
