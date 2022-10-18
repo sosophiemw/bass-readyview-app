a58b63f6b1004a33d422c64088eb609c4ba54b60
David Ward <david.a.ward@duke.edu>
2022-10-18 16:57:37 -0400

    Add documentation describing accessing the serial port and interpreting its data.

--- serial_port_access.py
+++ serial_port_access.py
@@ -4,17 +4,16 @@ import serial
 ser = serial.Serial(port='COM4', baudrate=115200, bytesize=serial.EIGHTBITS,
                     parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
 ser.write("[1D100\r".encode('utf-8'))
-# ser.open()
-while True:
-    # time.sleep(0.1)
-    # s = ser.read(1)
-    # hex_string = s.hex()
-    # dec = int(hex_string, 16)
-    # if dec < 128:
-    #     ascii = chr(dec)
-    # else:
-    #     ascii = ""
-    # print("Hex {} / Decimal {} / Char {}".format(hex_string, dec, ascii))
 
+# For continuous access
+while True:
     s = ser.readline()
     print(s)
+
+# Analyze a string
+# s = ser.readline()
+# print(s)
+# data_string = s.decode("utf-8")
+# print(data_string)
+# data = data_string.split(",")
+# print(data)
